30d2bd5e9f5849e74ec34c1abe817b26df2c33c3
IvanCarrera <ivanvilac@gmail.com>
2025-07-21 14:02:18 +0200

    Creacion de la estructura del proyecto y sus ficheros

---
+++ src/main/java/es/cic25/proy008/controller/EscuderiaController.java
@@ -0,0 +1,26 @@
+package es.cic25.proy008.controller;
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.cic25.proy008.model.Escuderia;
+// import es.cic25.proy008.service.EscuderiaService;
+
+@RestController
+@RequestMapping("/escuderias")
+public class EscuderiaController {
+
+    
+
+}
---
+++ src/main/java/es/cic25/proy008/repository/EscuderiaRepository.java
@@ -0,0 +1,5 @@
+package es.cic25.proy008.repository;
+
+public class EscuderiaRepository {
+
+}
