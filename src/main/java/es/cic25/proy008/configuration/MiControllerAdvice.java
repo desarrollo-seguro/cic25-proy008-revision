1de02084991215619d855379f1a439b6917fac78
IvanCarrera <ivanvilac@gmail.com>
2025-07-24 09:56:07 +0200

    Comentar el archivo MiControllerAdvice

--- src/main/java/es/cic25/proy008/configuration/MiControllerAdvice.java
+++ src/main/java/es/cic25/proy008/configuration/MiControllerAdvice.java
@@ -1,18 +1,18 @@
-package es.cic25.proy008.configuration;
+// package es.cic25.proy008.configuration;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.ResponseStatus;
 
-import es.cic25.proy008.controller.ModificacionSegurityException;
+// import es.cic25.proy008.controller.ModificacionSegurityException;
 
-@ControllerAdvice
-public class MiControllerAdvice {
+// @ControllerAdvice
+// public class MiControllerAdvice {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ModificacionSegurityException.class)
-    public void controloModificacion() {
+//     @ResponseStatus(HttpStatus.BAD_REQUEST)
+//     @ExceptionHandler(ModificacionSegurityException.class)
+//     public void controloModificacion() {
        
-    }
-}
+//     }
+// }
